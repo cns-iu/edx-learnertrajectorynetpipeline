32d9bdd73da4b7c447b7fef38ef7a70aaf46282d
mginda <michael.ginda@gmail.com>
2018-02-05 14:22:08 -0500

    Cleaned-up script
    Cleaned-up script to remove testing features, updated inline comments.

--- edX-eventLogFormatter.R
+++ edX-eventLogFormatter.R
@@ -39,6 +39,7 @@
 #   2017.09.29. Updating file for sharing
 #   2017.10.19. Updated file create function to produce event trajectory logs from a list of student IDs
 #   2017.10.31. Updated function to maintain only relevant fields
+#   2018.02.05. Cleaned Up script
 #
 ## ===================================================== ##
 
@@ -50,15 +51,15 @@ rm(list=ls())
 start <-  proc.time() #save the time (to compute elapsed time of script)
 
 ## _Load required packages #####
-require("ndjson")     #needed to read the non-standard JSON log files (NDJSON format)
 require("jsonlite")   #for working with JSON files (esp. read and write)
-require("tcltk2")     #for OS independant GUI file and folder selection
+require("ndjson")     #needed to read the non-standard JSON log files (NDJSON format)
+require("tcltk2")     #for OS independent GUI file and folder selection
 
 ####Functions 
 #logCapture 
 ##The logCapture function is a modification of code provided by Purdue University team
-##to allow mass extracting individual set of student logs based on known set of student IDs for an 
-##edX course. The function creates a unique log file for each student ID in the list, 
+##to allow mass extracting individual student's event logs from course event logs, based on known set 
+##of student IDs for an edX course. The function creates a unique log file for each student ID in the list, 
 ##saved as either a JSON or CSV formatted file. The function currently set up to save as CSV,
 ##alternatively can be set for user defined action such as format=T csv if format=F, JSON set up possible.
 
@@ -67,6 +68,7 @@ logCapture <- function(curUserIDS,eventLog,fileList,path){
   numLogFiles <- length(fileList) 
   for(j in 1:numStudents){
     curUser <- curUserIDS[j]
+    message("Processing student ", j, " of ", numStudents)
     for(i in 1:numLogFiles){
       curFileName <- fileList[i] 
       #print update message to console
@@ -89,7 +91,7 @@ logCapture <- function(curUserIDS,eventLog,fileList,path){
                                        "event.submitted_at","event.feedback","event.feedback_text",
                                        "event.rubric.content_hash","event.score_type","event.scored_at",
                                        "event.scorer_id"))
-    ###Not used columns that could be useful for other courses
+    ###Unused columns that could be useful for other courses
     #event.correctness	event.hint_label	event.hints.0.text	
     #event.module_id	event.problem_part_id	event.question_type	
     #event.trigger_type	event.enrollment_mode	event.social_network	
@@ -104,21 +106,14 @@ logCapture <- function(curUserIDS,eventLog,fileList,path){
 }
 
 ######### Main ########## 
-#Test set of individual userIDs for students in an edX Course whose event data should be extracted
-#Test code for function
-d <- data.frame(matrix(ncol = 1, nrow = 6))
-names(d) <- c("ids")
-d$ids <-  c("229670106","798100575","230119567","211115035","77747279","799212761")
-curUserIDS <- d$ids
-rm(d)
-
-eventLog <- NULL
-
 #Creates paths used to locate directory for research data sets and save processing outputs
-path_data <- c("Z:/research/17-Boeing/data/edx/MITProfessionalX_SysEngxB1_3T2016/events")
-#path_data = tclvalue(tkchooseDirectory())
-path_output <- c("Z:/research/17-Boeing/data/edx/MITProfessionalX_SysEngxB1_3T2016/studentevents/")
-#path_output = tclvalue(tkchooseDirectory())
+path_data = tclvalue(tkchooseDirectory())
+path_output = paste0(tclvalue(tkchooseDirectory()),"/")
+
+#List of Users IDs extracted from student user database
+d <- read.csv("FILENAME",header=T)
+curUserIDS <-d$id #this converts dataframe of user ids to integer list needed for the function
+eventLog <- NULL
 
 ## _Build list of all event files for course####
 #Store all the filenames of JSON formatted edX event logs within a user selected directory 
