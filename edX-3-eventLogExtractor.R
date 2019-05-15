6ba5abe3204735781eaae8858d05705abb5180e3
Ginda <mginda@iu.edu>
2019-05-15 11:07:31 -0400

    copyright

--- edX-3-eventLogExtractor.R
+++ edX-3-eventLogExtractor.R
@@ -3,7 +3,7 @@
 #               student users 
 # Project:      edX learner trajectory analysis
 # 
-#     Copyright 2017-2018 Michael Ginda & Krishna Madhavan
+#     Copyright 2017-2019 Michael Ginda & Krishna Madhavan
 #     Licensed under the Apache License, Version 2.0 (the "License");
 #     you may not use this file except in compliance with the License.
 #     You may obtain a copy of the License at
